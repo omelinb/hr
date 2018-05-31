20ed553888ea82355ce1ee52a28b0fa9c63c83d1
Boris Omelin <elros.ob@gmail.com>
2018-05-31 17:13:24 +0300

    Added basic auth

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('employees.urls')),
 ]
